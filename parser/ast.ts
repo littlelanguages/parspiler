91c03fcb2374bc3c1ed7e28f84da7cdadf14ad64
Graeme Lockley <graemel@no9.co.za>
2020-09-13 08:04:44 +0200

    feature: create AST

---
+++ parser/ast.ts
@@ -0,0 +1,56 @@
+import { Location, combine } from "./location.ts";
+
+export type Definition = {
+  tag: "Definition";
+  uses: LiteralString;
+  productions: Array<Production>;
+};
+
+export type Production = {
+  tag: "Production";
+  name: IdentifierReference;
+  expr: Expr;
+};
+
+export type Expr =
+  | SequenceExpr
+  | ParenExpr
+  | ManyExpr
+  | OptionalExpr
+  | LiteralString
+  | IdentifierReference;
+
+export type SequenceExpr = {
+  tag: "SequenceExpr";
+  factors: Array<Expr>;
+};
+
+export type ParenExpr = {
+  tag: "ParenExpr";
+  location: Location;
+  value: Expr;
+};
+
+export type ManyExpr = {
+  tag: "ManyExpr";
+  location: Location;
+  value: Expr;
+};
+
+export type OptionalExpr = {
+  tag: "OptionalExpr";
+  location: Location;
+  value: Expr;
+};
+
+export type LiteralString = {
+  tag: "LiteralString";
+  location: Location;
+  value: string;
+};
+
+export type IdentifierReference = {
+  tag: "ID";
+  location: Location;
+  id: string;
+};
