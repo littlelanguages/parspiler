9039804868d3270e55d6efe92c868e6c2076cabf
Graeme Lockley <graemel@no9.co.za>
2020-09-14 07:40:01 +0200

    feature: rather combine all of the scanner errors into a single error

--- parser/errors.ts
+++ parser/errors.ts
@@ -5,8 +5,15 @@ import { Location } from "./location.ts";
 export type Errors = Array<ErrorItem>;
 
 export type ErrorItem =
-  | ScanpilerErrors.ErrorItem
+  | ScannerDefinitionError
   | ScannerDefinitionFileDoesNotExistError;
+
+export type ScannerDefinitionError = {
+  tag: "ScannerDefinitionError";
+  location: Location;
+  fileName: string;
+  errors: ScanpilerErrors.Errors;
+};
 
 export type ScannerDefinitionFileDoesNotExistError = {
   tag: "ScannerDefinitionFileDoesNotExistError";
@@ -19,13 +26,25 @@ export function asDoc(
   fileName: string | undefined = undefined,
 ): PP.Doc {
   switch (errorItem.tag) {
+    case "ScannerDefinitionError":
+      return PP.vcat([
+        PP.hcat([
+          "Unable to load the scanner definition ",
+          errorItem.fileName,
+          ScanpilerErrors.errorLocation(errorItem.location, fileName),
+        ]),
+        PP.nest(
+          2,
+          PP.vcat(errorItem.errors.map((e) =>
+            ScanpilerErrors.asDoc(e, errorItem.fileName)
+          )),
+        ),
+      ]);
     case "ScannerDefinitionFileDoesNotExistError":
       return PP.hcat([
         "Unable to read the scanner definition file ",
         errorItem.name,
         ScanpilerErrors.errorLocation(errorItem.location, fileName),
       ]);
-    default:
-      return ScanpilerErrors.asDoc(errorItem);
   }
 }
