7ae421d80595c67aa8893c1dde3329f3658b1fb0
Graeme Lockley <graemel@no9.co.za>
2020-09-15 06:26:04 +0200

    feature: add isLeft and isRight

--- data/either.ts
+++ data/either.ts
@@ -1,1 +1,11 @@
+import { Either } from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-either/0.0.1/mod.ts";
+
 export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-either/0.0.1/mod.ts";
+
+export function isLeft<A, B>(e: Either<A, B>): boolean {
+  return e.either((_) => true, (_) => false);
+}
+
+export function isRight<A, B>(e: Either<A, B>): boolean {
+  return e.either((_) => false, (_) => true);
+}
