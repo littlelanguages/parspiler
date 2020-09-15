103e1b2ae29f326d5c71a99e1388acc8c07b4aa4
Graeme Lockley <graemel@no9.co.za>
2020-09-15 21:17:41 +0200

    test: add scenarios however code still needs to be built

--- parser/dynamic_test.ts
+++ parser/dynamic_test.ts
@@ -1,10 +1,16 @@
-import { left } from "../data/either.ts";
+import { isRight, left } from "../data/either.ts";
 
 import * as Assert from "../testing/asserts.ts";
 import * as Errors from "./errors.ts";
 
 import { translate } from "./dynamic.ts";
 import { range } from "./location.ts";
+import { Definition } from "../cfg/definition.ts";
+import { Dynamic, Definition as LADefinition } from "../scanpiler.ts";
+
+const scannerDefinition = Dynamic
+  .translate(Deno.readTextFileSync("./parser/scanner.ll"))
+  .either((_) => new LADefinition.Definition(), (d) => d);
 
 Deno.test("dynamic - scanner file does not exist", () => {
   assertTranslateErrors('uses "not.exists.ll";', [
@@ -19,8 +25,48 @@ Deno.test("dynamic - scanner file does not exist", () => {
 Deno.test("dynamic - scanner file exists", async () => {
   const translation = await translate('uses "./parser/scanner.ll";');
 
-  Assert.assert(translation.either((_) => false, (_) => true));
+  Assert.assert(isRight(translation));
 });
+
+Deno.test(
+  {
+    name: "dynamic - an error in the scanner file propogates",
+    ignore: true,
+    fn() {
+    },
+  },
+);
+
+Deno.test({
+  name: "dynamic - reference to terminal symbol",
+  ignore: true,
+  fn() {
+    assertTranslation(
+      'uses "./parser/scanner.ll";\n' + "Program: {Identifier};",
+      new Definition(scannerDefinition),
+    );
+  },
+});
+
+Deno.test({
+  name: "dynamic - reference to non-terminal symbol",
+  ignore: true,
+  fn() {
+  },
+});
+
+Deno.test({
+  name: "dynamic - non-terminal and terminal symbol clash",
+  ignore: true,
+  fn() {
+  },
+});
+
+async function assertTranslation(content: string, definition: Definition) {
+  const x = await translate(content);
+
+  Assert.assert(isRight(x));
+}
 
 async function assertTranslateErrors(content: string, errors: Errors.Errors) {
   const x = await translate(content);
