7b0551d6b3270d2a3a44dc1bc1cd459958210fc5
Graeme Lockley <graemel@no9.co.za>
2020-09-15 06:01:12 +0200

    feature: dynamic - scanner file does not exist"

--- parser/dynamic.ts
+++ parser/dynamic.ts
@@ -2,12 +2,34 @@ import { Either, right, left } from "../data/either.ts";
 
 import * as Parser from "./parser.ts";
 import * as AST from "./ast.ts";
-import { Errors } from "./errors.ts";
+import * as Errors from "./errors.ts";
+import { Definition } from "../cfg/definition.ts";
+import { Scanner, Dynamic } from "../scanpiler.ts";
+import { dropLeft, dropRight } from "../data/string.ts";
 
-export function translate(input: string): Promise<Either<Errors, string>> {
-  return Promise.resolve(tt(input));
-}
+export function translate(
+  input: string,
+): Promise<Either<Errors.Errors, Definition>> {
+  const xx = Parser.parseDefinition(input, new AST.Visitor());
 
-function tt(input: string): Either<Errors, string> {
-  return Parser.parseDefinition(input, new AST.Visitor()).map((_) => "");
+  return xx.either(
+    (l) => Promise.resolve(left(l)),
+    async (r) => {
+      const fileName = dropRight(1, dropLeft(1, r.uses.value));
+      try {
+        const input = await Deno.readTextFile(fileName);
+        return Dynamic.translate(input);
+      } catch (_) {
+        return left(
+          [
+            {
+              tag: "ScannerDefinitionFileDoesNotExistError",
+              location: r.uses.location,
+              name: fileName,
+            },
+          ] as Errors.Errors,
+        );
+      }
+    },
+  ) as Promise<Either<Errors.Errors, Definition>>;
 }
--- parser/dynamic_test.ts
+++ parser/dynamic_test.ts
@@ -1,7 +1,23 @@
-import * as AST from "../testing/asserts.ts";
+import { left } from "../data/either.ts";
+
+import * as Assert from "../testing/asserts.ts";
+import * as Errors from "./errors.ts";
 
 import { translate } from "./dynamic.ts";
+import { range } from "./location.ts";
 
-// Deno.test("dynamic - scanner file does not exist", () => {
-//     translate('uses "not.exists.ll";');
-// });
+Deno.test("dynamic - scanner file does not exist", () => {
+  assertTranslateErrors('uses "not.exists.ll";', [
+    {
+      tag: "ScannerDefinitionFileDoesNotExistError",
+      location: range(5, 1, 6, 19, 1, 20),
+      name: "not.exists.ll",
+    },
+  ]);
+});
+
+async function assertTranslateErrors(content: string, errors: Errors.Errors) {
+  const x = await translate(content);
+
+  Assert.assertEquals(x, left(errors));
+}
