148ffdf574f81bbca5a90465d097981a8d3a2dea
Graeme Lockley <graemel@no9.co.za>
2020-09-15 05:59:59 +0200

    refactor: move all reference to scanpiler to a single file

---
+++ scanpiler.ts
@@ -0,0 +1,3 @@
+export * as Dynamic from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/parser/dynamic.ts";
+export * as Errors from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/parser/errors.ts";
+export * as Scanner from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/la/definition.ts";
