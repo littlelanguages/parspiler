d35f67514f77d0b797270cdc64426a375fa68f3a
Graeme Lockley <graemel@no9.co.za>
2020-09-15 06:27:03 +0200

    feature: import Definition

--- scanpiler.ts
+++ scanpiler.ts
@@ -1,3 +1,4 @@
+export * as Definition from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/la/definition.ts";
 export * as Dynamic from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/parser/dynamic.ts";
 export * as Errors from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/parser/errors.ts";
 export * as Scanner from "https://raw.githubusercontent.com/littlelanguages/scanpiler/0.1.0/la/definition.ts";
