9840c80add7573b04329925876da013c59619266
Graeme Lockley <graemel@no9.co.za>
2020-09-16 07:22:45 +0200

    fix: return parspiler definition

--- parser/dynamic.ts
+++ parser/dynamic.ts
@@ -1,11 +1,11 @@
 import { Either, right, left } from "../data/either.ts";
+import { dropLeft, dropRight } from "../data/string.ts";
 
 import * as Parser from "./parser.ts";
 import * as AST from "./ast.ts";
 import * as Errors from "./errors.ts";
 import { Definition } from "../cfg/definition.ts";
-import { Scanner, Dynamic } from "../scanpiler.ts";
-import { dropLeft, dropRight } from "../data/string.ts";
+import { Dynamic } from "../scanpiler.ts";
 
 export function translate(
   input: string,
@@ -14,22 +14,22 @@ export function translate(
 
   return xx.either(
     (l) => Promise.resolve(left(l)),
-    async (r) => {
-      const fileName = dropRight(1, dropLeft(1, r.uses.value));
+    async (ast) => {
+      const fileName = dropRight(1, dropLeft(1, ast.uses.value));
       try {
         const input = await Deno.readTextFile(fileName);
         return Dynamic.translate(input).mapLeft((e) => [{
           tag: "ScannerDefinitionError",
-          location: r.uses.location,
+          location: ast.uses.location,
           fileName: fileName,
           errors: e,
-        }]);
+        }]).map((scannerDefinition) => new Definition(scannerDefinition));
       } catch (_) {
         return left(
           [
             {
               tag: "ScannerDefinitionFileDoesNotExistError",
-              location: r.uses.location,
+              location: ast.uses.location,
               name: fileName,
             },
           ] as Errors.Errors,
