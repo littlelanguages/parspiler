558c33cfda40a4832b6547d8c7dfa2745361e8a5
Graeme Lockley <graemel@no9.co.za>
2020-09-16 07:24:36 +0200

    refactor: chain expressions

--- parser/dynamic.ts
+++ parser/dynamic.ts
@@ -10,31 +10,31 @@ import { Dynamic } from "../scanpiler.ts";
 export function translate(
   input: string,
 ): Promise<Either<Errors.Errors, Definition>> {
-  const xx = Parser.parseDefinition(input, new AST.Visitor());
-
-  return xx.either(
-    (l) => Promise.resolve(left(l)),
-    async (ast) => {
-      const fileName = dropRight(1, dropLeft(1, ast.uses.value));
-      try {
-        const input = await Deno.readTextFile(fileName);
-        return Dynamic.translate(input).mapLeft((e) => [{
-          tag: "ScannerDefinitionError",
-          location: ast.uses.location,
-          fileName: fileName,
-          errors: e,
-        }]).map((scannerDefinition) => new Definition(scannerDefinition));
-      } catch (_) {
-        return left(
-          [
-            {
-              tag: "ScannerDefinitionFileDoesNotExistError",
-              location: ast.uses.location,
-              name: fileName,
-            },
-          ] as Errors.Errors,
-        );
-      }
-    },
-  ) as Promise<Either<Errors.Errors, Definition>>;
+  return Parser
+    .parseDefinition(input, new AST.Visitor())
+    .either(
+      (l) => Promise.resolve(left(l)),
+      async (ast) => {
+        const fileName = dropRight(1, dropLeft(1, ast.uses.value));
+        try {
+          const input = await Deno.readTextFile(fileName);
+          return Dynamic.translate(input).mapLeft((e) => [{
+            tag: "ScannerDefinitionError",
+            location: ast.uses.location,
+            fileName: fileName,
+            errors: e,
+          }]).map((scannerDefinition) => new Definition(scannerDefinition));
+        } catch (_) {
+          return left(
+            [
+              {
+                tag: "ScannerDefinitionFileDoesNotExistError",
+                location: ast.uses.location,
+                name: fileName,
+              },
+            ] as Errors.Errors,
+          );
+        }
+      },
+    ) as Promise<Either<Errors.Errors, Definition>>;
 }
