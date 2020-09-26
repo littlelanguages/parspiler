fbc8e7f12904eb9802984f62d1ec4b4c5d5030d1
Graeme Lockley <graemel@no9.co.za>
2020-09-26 08:20:49 +0200

    refactor: replace function with arrow notation

--- data/array.ts
+++ data/array.ts
@@ -1,16 +1,10 @@
 import * as Set from "./set.ts";
 
-export function and(a: Array<boolean>): boolean {
-  return a.reduce((a, b) => a && b, true);
-}
+export const and = (a: Array<boolean>): boolean =>
+  a.reduce((a, b) => a && b, true);
 
-export function or(a: Array<boolean>): boolean {
-  return a.reduce((a, b) => a || b, false);
-}
+export const or = (a: Array<boolean>): boolean =>
+  a.reduce((a, b) => a || b, false);
 
-export function union<T>(ss: Array<Set<T>>): Set<T> {
-  return ss.reduce(
-    Set.union,
-    Set.emptySet as Set<T>,
-  );
-}
+export const union = <T>(ss: Array<Set<T>>): Set<T> =>
+  ss.reduce(Set.union, Set.emptySet as Set<T>);
