5054101e4480faccaefb1badcfe6e0d941ea9af2
Graeme Lockley <graemel@no9.co.za>
2020-09-30 06:38:04 +0200

    fix: remove code generator from build

--- .bin/build.sh
+++ .bin/build.sh
@@ -2,23 +2,3 @@
 
 deno run --allow-read --allow-write "https://raw.githubusercontent.com/littlelanguages/scanpiler-cli/main/mod.ts" deno --verbose parser/scanner.ll
 deno fmt parser/scanner.ts
-
-if [[ -e "test/parspiler/parser.ts" ]]
-then
-    deno fmt test/parspiler/parser.ts
-fi
-
-if [[ -e "test/parspiler/scanner.ts" ]]
-then
-    deno fmt test/parspiler/scanner.ts
-fi
-
-if [[ -e "test/simple/parser.ts" ]]
-then
-    deno fmt test/simple/parser.ts
-fi
-
-if [[ -e "test/simple/scanner.ts" ]]
-then
-    deno fmt test/simple/scanner.ts
-fi
