7cea249860ba53b95837e997356984dcf04980c8
Graeme Lockley <graemel@no9.co.za>
2020-09-30 06:38:21 +0200

    chore: upgrade to either 0.1.0

--- data/either.ts
+++ data/either.ts
@@ -1,9 +1,1 @@
-import { Either } from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-either/0.0.1/mod.ts";
-
-export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-either/0.0.1/mod.ts";
-
-export const isLeft = <A, B>(e: Either<A, B>): boolean =>
-  e.either((_) => true, (_) => false);
-
-export const isRight = <A, B>(e: Either<A, B>): boolean =>
-  e.either((_) => false, (_) => true);
+export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-either/0.1.0/mod.ts";
