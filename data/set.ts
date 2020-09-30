07cd8ad61b9f63c58c64023e7c14fbf2883322a8
Graeme Lockley <graemel@no9.co.za>
2020-09-30 06:50:38 +0200

    chore: upgrade set to 0.1.0

--- data/set.ts
+++ data/set.ts
@@ -1,6 +1,1 @@
-export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-set/0.0.1/mod.ts";
-
-import * as Set from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-set/0.0.1/mod.ts";
-
-export const filter = <S>(p: (e: S) => boolean, s: Set<S>) =>
-  Set.setOf([...s].filter(p));
+export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-data-set/0.1.0/mod.ts";
