5f4a9a6d6aed3fe35854412d0e2303e53da406ef
Graeme Lockley <graemel@no9.co.za>
2020-11-07 06:36:01 +0000

    refactor: format

--- mod.ts
+++ mod.ts
@@ -1,8 +1,5 @@
 export * from "./cfg/definition.ts";
 export * from "./parser/errors.ts";
 export { translate } from "./parser/dynamic.ts";
-export {
-  parseDefinition,
-  mkParser,
-} from "./parser/parser.ts";
+export { mkParser, parseDefinition } from "./parser/parser.ts";
 export { SyntaxError } from "./parser/parspiler-parser.ts";
