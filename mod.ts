d8fd7edba6977d468961609561b372fb60367ee2
Graeme Lockley <graemel@no9.co.za>
2020-11-11 04:11:04 +0000

    fix: use export type

--- mod.ts
+++ mod.ts
@@ -2,4 +2,4 @@ export * from "./cfg/definition.ts";
 export * from "./parser/errors.ts";
 export { translate } from "./parser/dynamic.ts";
 export { mkParser, parseDefinition } from "./parser/parser.ts";
-export { SyntaxError } from "./parser/parspiler-parser.ts";
+export type { SyntaxError } from "./parser/parspiler-parser.ts";
